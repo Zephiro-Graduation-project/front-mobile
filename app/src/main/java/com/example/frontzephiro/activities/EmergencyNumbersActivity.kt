91a952ddc9435e8a3967c0d67906e200c36cc52a
SantiagoYB <98602870+SantiagoYB@users.noreply.github.com>
2025-03-18 23:05:44 -0500

    Pantalla de numeros de emergencia

---
+++ app/src/main/java/com/example/frontzephiro/activities/EmergencyNumbersActivity.kt
@@ -0,0 +1,38 @@
+package com.example.frontzephiro.activities
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.frontzephiro.R
+import com.google.android.material.card.MaterialCardView
+
+class EmergencyNumbersActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_emergency_numbers)
+
+        val cardBogota = findViewById<MaterialCardView>(R.id.cardBogota)
+        val cardNacional = findViewById<MaterialCardView>(R.id.cardNacional)
+        val cardAyudaLinea = findViewById<MaterialCardView>(R.id.cardAyudaLinea)
+
+        cardBogota.setOnClickListener {
+            val phoneNumber = "1234567890"
+            val dialIntent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:$phoneNumber"))
+            startActivity(dialIntent)
+        }
+
+        cardNacional.setOnClickListener {
+            val phoneNumber = "0987654321"
+            val dialIntent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:$phoneNumber"))
+            startActivity(dialIntent)
+        }
+
+        cardAyudaLinea.setOnClickListener {
+            val url = "google.com"
+            val webIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+            startActivity(webIntent)
+        }
+    }
+}
