15048de2bce5e7f5035e06b7149030a8231e6e8b
Shermann103 <hhermann103@gmail.com>
2025-04-30 14:14:44 -0500

    esthetic changes in graph and profile screens
    - the graphs are presented in cards
    - the background is in gradient colors
    - new generic profile image

--- app/src/main/java/com/example/frontzephiro/activities/GraficaActivity.kt
+++ app/src/main/java/com/example/frontzephiro/activities/GraficaActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.LinearLayout
 import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -32,6 +33,12 @@ class GraficaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_grafica)
+
+        val backContainer = findViewById<LinearLayout>(R.id.backContainer)
+
+        backContainer.setOnClickListener {
+            onBackPressed()
+        }
 
         progressBar  = findViewById(R.id.progressBar)
         anxietyChart = findViewById(R.id.anxietyChart)
